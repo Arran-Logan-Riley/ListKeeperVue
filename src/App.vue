5a96cd6739ba13acc1dae5b987644b8df13c9f48
Arran <plazmapie335@gmail.com>
2023-05-17 14:03:12 +1200

    First iteration of list keeper

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<ListItem/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/toDoListComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListItem
   }
 }
 </script>
---
+++ src/components/toDoListComp.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <List-item class="card" v-for="(item, index) in items"
+        :key="index"
+        :listItemTitle="item.title"
+        :cardFillText="item.text"
+        :index="index"
+        >
+        {{item.title}}
+        {{item.text}}
+    </List-item>
+    </div>
+</template>
+
+<style scoped>
+.card{
+    background-color: #f8f8f8;
+    border-radius: 4px;
+    padding: 50px;
+    margin-bottom: 10px;
+    margin-right: 10px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+h1{
+    font-size: 18px;
+    margin-bottom: 5px;
+}
+
+p{
+    font-size: 14px;
+    color: #555555;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return{
+            items: [
+                {title:'Title 1',text:'text 1'},
+                {title:'Title 2',text:'text 2'},
+                {title:'Title 3',text:'text 3'},
+            ]
+        };
+    }
+}
+</script>
