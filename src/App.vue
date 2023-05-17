3d03a0f091f8b063e069780f34bd95b641bdbb5d
Arran <plazmapie335@gmail.com>
2023-05-17 14:29:46 +1200

    First functoonal version.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,15 @@
 <template>
-<ListItem/>
+  <div>
+    <todo-list></todo-list>
+  </div>
 </template>
 
 <script>
-import ListItem from './components/toDoListComp.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
-  name: 'App',
-  components: {
-    ListItem
+  components:{
+    TodoList
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <h1>To-Do List</h1>
+      <input v-model="newTodo" type="text" placeholder="Add a new task">
+      <button @click="addTodo">ADD</button>
+      <ul>
+        <li v-for="(todo, index) in todos"
+        :key="index"
+        >{{ todo }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data(){
+      return {
+        todos:[],
+        newTodos: ''
+      };
+    },
+    methods:{
+      addTodo(){
+        //newTodo is the text field. 
+        if(this.newTodo.trim() !== ''){
+          this.todos.push(this.newTodo);
+          this.newTodo = '';
+        }
+      },
+      removeTodo(index){
+        this.todos.splice(index, 1)
+      }
+    }
+  }
+  </script>
